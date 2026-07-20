6f6f3432acbe7ebeea4fccdf1d16ab2d2372b58c
Viveka-123 <chitakulavivekanandana@gmail.com>
2026-07-20 11:39:54 +0530

    Update base path conditionally for Vercel and GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Viveka-QA-Analyst/',
+  base: process.env.VERCEL ? '/' : '/Viveka-QA-Analyst/',
   plugins: [react()],
 })
